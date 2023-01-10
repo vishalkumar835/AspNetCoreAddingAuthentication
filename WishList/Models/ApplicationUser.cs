ca5a4228d7e337ef761761207cde0463691e14dc
vishal <vg.vishu99@gmail.com>
2023-01-10 10:45:43 +0530

    ApplicationUser

--- WishList/Models/ApplicationUser.cs
+++ WishList/Models/ApplicationUser.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
 
 namespace WishList.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public virtual ICollection<Item> Items { get; set; }
     }
 }
